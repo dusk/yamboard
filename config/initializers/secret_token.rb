308b725a4df82bf070f38c479a530daffb2a572b
Alex Annese <alex@yammer-inc.com>
2010-11-03 19:19:32 -0700

    Now its a rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yamboard::Application.config.secret_token = '2fd62856f99720053558bffa5fbefb4597c394e2719a0a4e024d7ece3d05b44a645f7e942f0b1b1f7dce2bafa1c5007510d9c998fe1229f73846728f8124a366'
