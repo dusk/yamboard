95880574a16e9fd4dde8fe208bbefafe48787339
Josh Ferguson <jferguson@yammer-inc.com>
2010-11-03 20:33:04 -0700

    time zones to time slice stat?

--- app/models/workfeed/time_slice_stat.rb
+++ app/models/workfeed/time_slice_stat.rb
@@ -3,11 +3,16 @@ module Workfeed
     establish_connection(ActiveRecord::Base.configurations["workfeed"])
     
     class << self
+      
+      #
+      # To get the past week
+      # Workfeed::TimeSliceStat.get_stats(8.days.ago, Time.now)
+      #
       def get_stats(start_time, end_time)
         where([%{
           slice_time >= ? 
           AND slice_time < ? 
-          AND EXTRACT('hour' FROM slice_time) = 0 
+          AND EXTRACT('hour' FROM slice_time) = 7
           AND EXTRACT('minute' FROM slice_time) = 0
         }.squish, start_time.beginning_of_day, end_time.beginning_of_day]).all
       end
